23f03deeba79fabafebbc1e66cefe09f60c3e931
makelannika <makelannika@gmail.com>
2024-07-25 21:45:36 +0300

    first checks

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,13 +6,15 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 13:24:23 by amakela           #+#    #+#             */
-/*   Updated: 2024/07/24 15:28:57 by amakela          ###   ########.fr       */
+/*   Updated: 2024/07/25 21:30:46 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include <stdio.h>
+# include "../libft/include/libft.h"
+# include <stdio.h>
+# include <errno.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include "../include/cub3d.h"
 
-int	main(void)
+int	check_extension(char *arg)
 {
-	printf("works\n");
+	int     len;
+
+	len = ft_strlen(arg);
+	return (ft_strncmp(&arg[len - 4], ".cub", 5));
+}
+
+int	err(char *str)
+{
+	ft_printf(2, "%s\n", str);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+		return (err("error: invalid arguments"));
+	if (check_extension(argv[1]))
+		return (err("error: invalid file type"));
+	ft_printf(1, "OK\n");
 	return (0);
 }
