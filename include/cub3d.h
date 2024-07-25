248a20cd71f572e23948ad58d0f6ca12f17ccefb
makelannika <makelannika@gmail.com>
2024-07-25 21:50:46 +0300

    unnecessary headers removed

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 13:24:23 by amakela           #+#    #+#             */
-/*   Updated: 2024/07/25 21:30:46 by amakela          ###   ########.fr       */
+/*   Updated: 2024/07/25 21:50:09 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 # define CUB3D_H
 
 # include "../libft/include/libft.h"
-# include <stdio.h>
-# include <errno.h>
 
 #endif
