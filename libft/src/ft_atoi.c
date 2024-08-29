253cfe53b49c0015c1f9df45f29a4bdf2ae3845c
makelannika <makelannika@gmail.com>
2024-08-29 20:49:06 +0300

    parsing

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,43 +6,35 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 11:39:48 by amakela           #+#    #+#             */
-/*   Updated: 2023/11/16 15:51:09 by amakela          ###   ########.fr       */
+/*   Updated: 2024/08/29 19:31:10 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	overflow(int neg);
-
 int	ft_atoi(const char *str)
 {
-	int		i;
 	long	nbr;
-	int		neg;
-	long	prev;
+	int		sign;
+	int		prev;
 
-	i = 0;
 	nbr = 0;
-	neg = 1;
-	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-		i ++;
-	if ((str[i] == '-') || (str[i] == '+'))
-	{
-		if (str[i] == '-')
-			neg = -1;
-		i ++;
-	}
-	while (str[i] >= 48 && str[i] <= 57)
+	sign = 1;
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str == '-')
+		sign = -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
 	{
 		prev = nbr;
-		nbr = 10 * nbr + str[i++] - '0';
+		nbr = 10 * nbr + *str - '0';
 		if (prev > nbr)
-			return (overflow(neg));
+		{
+			if (sign == -1)
+				return (0);
+			return (-1);
+		}
+		str++;
 	}
-	return ((int)nbr * neg);
-}
-
-static int	overflow(int neg)
-{
-	if (neg == -1)
-		return (0);
-	return (-1);
+	return ((int)nbr * sign);
 }
--- src/main.c
+++ src/main.c
@@ -21,59 +21,132 @@ int	copy_path(char *line, char *id)
 	return (0);
 }
 
-int	copy_color(char *line, int *id)
+int	count_commas(char *str)
+{
+	int	commas;
+
+	commas = 0;
+	while (*str != '\0') {
+		if (*str == ',')
+			commas++;
+		str++;
+	}
+	return (commas);
+}
+
+int	validate_color(char *str)
+{
+	int	i;
+	int	color;
+
+	i = 0;
+	while (str[i])
+	{
+		if ((str[i] < '0' || str[i] > '9') && str[i] != '\n')
+		{
+			err("Error: invalid element");
+			return (-1);
+		}
+		i++;
+	}
+	color = ft_atoi(str);
+	if (color < 0 || color > 255)
+	{
+		err("Error: invalid element");
+		return (-1);
+	}
+	return (color);
+}
+
+int	copy_color(char *str, int *id)
 {
 	char **rgb;
 	
-	while (*line == ' ')
-		line++;
-	//check the rgb is a good string
-	rgb = ft_split(line, ',');
+	if (count_commas(str) != 2)
+		return (err("Error: invalid element"));
+	rgb = ft_split(str, ',');
 	if (!rgb)
 		return (err("Error: malloc failed in split"));
-	id[0] = ft_atoi(rgb[0]);
-	id[1] = ft_atoi(rgb[1]);
-	id[2] = ft_atoi(rgb[2]);
+	id[0] = validate_color(rgb[0]);
+	id[1] = validate_color(rgb[1]);
+	id[2] = validate_color(rgb[2]);
+	if (id[0] == -1 || id[1] == -1 || id[2] == -1)
+	{
+		// free rgb
+		return (1);
+	}
+	// free rgb
 	return (0);
 }
 
-int	check_identifier(char *line, t_cub *data)
+int	check_identifier(char **element, t_cub *data)
 {
-	if (!ft_strncmp("NO ", line, 3))
-		return (copy_path(line, data->no));
-	if (!ft_strncmp("WE ", line, 3))
-		return (copy_path(line, data->we));
-	if (!ft_strncmp("EA ", line, 3))
-		return (copy_path(line, data->ea));
-	if (!ft_strncmp("SO ", line, 3))
-		return (copy_path(line, data->so));
-	if (!ft_strncmp("F ", line, 2))
-		return (copy_color(&line[1], data->floor));
-	if (!ft_strncmp("C ", line, 2))
-		return (copy_color(&line[1], data->ceiling));
-	return (1);
+	if (!ft_strncmp("NO", element[0], 3))
+		data->no = element[1];
+	else if (!ft_strncmp("WE", element[0], 3))
+		data->we = element[1];
+	else if (!ft_strncmp("EA", element[0], 3))
+		data->ea = element[1];
+	else if (!ft_strncmp("SO", element[0], 3))
+		data->so = element[1];
+	else if (!ft_strncmp("F", element[0], 2))
+		return (copy_color(element[1], data->floor));
+	else if (!ft_strncmp("C", element[0], 2))
+		return (copy_color(element[1], data->ceiling));
+	else 
+		return (1);
+	return (0);
 }
+
+int	parse_element(t_cub *data, char *line)
+{
+	char	**element;
+
+	element = ft_split(line, ' ');
+	if (!element)
+		return (err("Error: malloc failed"));
+	if (check_identifier(element, data))
+	{
+		// free split
+		return (1);
+	}
+	// free split
+	return (0);
+}
+
+// int	parse_map(char *line, int fd)
+// {
+	
+// }
 
 int	parse_file(t_cub *data, char *file)
 {
 	int		fd;
 	char	*line;
 
-	ft_printf(1, "%d\n", data->floor[0]);
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return (err("Error: open failed :("));
-	line = get_next_line(fd); //check line
-	while (line && ft_strncmp(line, " ", 1) && ft_strncmp(line, "1", 1))
+	line = get_next_line(fd); // add malloc check for gnl
+	while (line)
 	{
-		if (ft_strncmp(line, "\n", 1))
-		{
-			line = get_next_line(fd);
-			continue ;
+		if (ft_strchr("NSWEFC", *line)) {
+			if (parse_element(data, line))
+			{
+				free(line);
+				return (1);
+			}
 		}
-		if(check_identifier(line, data))
-			return (err("Error:"));
-		
+		// else if (ft_strchr(" 1", *line)) {
+		// 	return (parse_map(line, fd));
+		// }
+		else if (*line != '\n')
+		{
+			free(line);
+			return (err("Error: forbidden content in .cub file"));
+		}
+		free(line);
+		line = get_next_line(fd); // malloc check
 	}
 	return (0);
 }
@@ -87,7 +160,8 @@ int	main(int argc, char **argv)
 		return (err("Error: invalid argument"));
 	if (parse_file(&data, argv[1]))
 		return (1);
+	ft_printf(1, "floor: %d, %d, %d\n", data.floor[0], data.floor[1], data.floor[2]);
+	ft_printf(1, "ceiling: %d, %d, %d\n", data.ceiling[0], data.ceiling[1], data.ceiling[2]);
 	ft_printf(1, "OK\n");
 	return (0);
 }
-
