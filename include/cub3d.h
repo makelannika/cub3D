fbd88080b5f8715d171566ff847e11933d67cf7d
makelannika <makelannika@gmail.com>
2024-08-29 21:56:23 +0300

    most leaks handled

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 13:24:23 by amakela           #+#    #+#             */
-/*   Updated: 2024/08/29 17:22:16 by amakela          ###   ########.fr       */
+/*   Updated: 2024/08/29 21:09:42 by amakela          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ typedef struct s_cub
 	char	*ea;
 	int		floor[3];
 	int		ceiling[3];
+	int		elements_found;
 } t_cub;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -13,12 +13,19 @@ int	err(char *str)
 	ft_printf(2, "%s\n", str);
 	return (1);
 }
-int	copy_path(char *line, char *id)
+
+int	free_str_array(char **array)
 {
-	id = ft_substr(line, 0, ft_strlen(line) - 1);
-	if (!id)
-		return (err("Error: malloc failed in substr"));
-	return (0);
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		if (array[i])
+			free(array[i++]);
+	}
+	free(array);
+	return (1);
 }
 
 int	count_commas(char *str)
@@ -43,18 +50,12 @@ int	validate_color(char *str)
 	while (str[i])
 	{
 		if ((str[i] < '0' || str[i] > '9') && str[i] != '\n')
-		{
-			err("Error: invalid element");
-			return (-1);
-		}
+			return (err("Error: invalid element"));
 		i++;
 	}
 	color = ft_atoi(str);
 	if (color < 0 || color > 255)
-	{
-		err("Error: invalid element");
-		return (-1);
-	}
+		return (err("Error: invalid element"));
 	return (color);
 }
 
@@ -71,53 +72,70 @@ int	copy_color(char *str, int *id)
 	id[1] = validate_color(rgb[1]);
 	id[2] = validate_color(rgb[2]);
 	if (id[0] == -1 || id[1] == -1 || id[2] == -1)
-	{
-		// free rgb
-		return (1);
-	}
-	// free rgb
+		return (free_str_array(rgb));
+	free_str_array(rgb);
 	return (0);
 }
 
 int	check_identifier(char **element, t_cub *data)
 {
-	if (!ft_strncmp("NO", element[0], 3))
-		data->no = element[1];
-	else if (!ft_strncmp("WE", element[0], 3))
-		data->we = element[1];
-	else if (!ft_strncmp("EA", element[0], 3))
-		data->ea = element[1];
-	else if (!ft_strncmp("SO", element[0], 3))
-		data->so = element[1];
-	else if (!ft_strncmp("F", element[0], 2))
+	char	*copy;
+	
+	if (!ft_strncmp("F", element[0], 2))
 		return (copy_color(element[1], data->floor));
 	else if (!ft_strncmp("C", element[0], 2))
 		return (copy_color(element[1], data->ceiling));
-	else 
+	copy = ft_strdup(element[1]);
+	if (!copy)
+		return (err("Error: malloc failed"));
+	if (!ft_strncmp("NO", element[0], 3))
+		data->no = copy;
+	else if (!ft_strncmp("WE", element[0], 3))
+		data->we = copy;
+	else if (!ft_strncmp("EA", element[0], 3))
+		data->ea = copy;
+	else if (!ft_strncmp("SO", element[0], 3))
+		data->so = copy;
+	else
+	{
+		free(copy);
 		return (1);
+	}
 	return (0);
 }
 
 int	parse_element(t_cub *data, char *line)
 {
 	char	**element;
+	int		len;
 
 	element = ft_split(line, ' ');
 	if (!element)
 		return (err("Error: malloc failed"));
-	if (check_identifier(element, data))
+	if (element[2])
 	{
-		// free split
-		return (1);
+		free_str_array(element);
+		return (err("Error: invalid element"));
 	}
-	// free split
+	len = ft_strlen(element[1]);
+	element[1][len - 1] = '\0';
+	if (check_identifier(element, data))
+		return (free_str_array(element));
+	free_str_array(element);
+	data->elements_found++;
 	return (0);
 }
 
-// int	parse_map(char *line, int fd)
-// {
-	
-// }
+int	parse_map(t_cub *data, char *line)
+{
+	if (data->elements_found != 6)
+	{
+		free(line);
+		return (err("Error: invalid .cub file content"));
+	}
+	free(line);
+	return (0);
+}
 
 int	parse_file(t_cub *data, char *file)
 {
@@ -137,9 +155,9 @@ int	parse_file(t_cub *data, char *file)
 				return (1);
 			}
 		}
-		// else if (ft_strchr(" 1", *line)) {
-		// 	return (parse_map(line, fd));
-		// }
+		else if (ft_strchr(" 1", *line)) {
+			return (parse_map(data, line));
+		}
 		else if (*line != '\n')
 		{
 			free(line);
@@ -151,6 +169,19 @@ int	parse_file(t_cub *data, char *file)
 	return (0);
 }
 
+int	free_data(t_cub *data)
+{
+	if (data->no)
+		free(data->no);
+	if (data->so)
+		free(data->so);
+	if (data->we)
+		free(data->we);
+	if (data->ea)
+		free(data->ea);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_cub data;
@@ -159,9 +190,14 @@ int	main(int argc, char **argv)
 	if (argc != 2 || check_extension(argv[1]))
 		return (err("Error: invalid argument"));
 	if (parse_file(&data, argv[1]))
-		return (1);
-	ft_printf(1, "floor: %d, %d, %d\n", data.floor[0], data.floor[1], data.floor[2]);
-	ft_printf(1, "ceiling: %d, %d, %d\n", data.ceiling[0], data.ceiling[1], data.ceiling[2]);
+		return (free_data(&data));
+	ft_printf(1, "NO: %s\n", data.no);
+	ft_printf(1, "SO: %s\n", data.so);
+	ft_printf(1, "WE: %s\n", data.we);
+	ft_printf(1, "EA: %s\n", data.ea);
+	ft_printf(1, "floor: %d,%d,%d\n", data.floor[0], data.floor[1], data.floor[2]);
+	ft_printf(1, "ceiling: %d,%d,%d\n", data.ceiling[0], data.ceiling[1], data.ceiling[2]);
 	ft_printf(1, "OK\n");
+	free_data(&data);
 	return (0);
 }
