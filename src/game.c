c92fed2641b0a1950e10cb4715e7772debbe349f
michael1nguyen1 <michael22nguyen@gmail.com>
2024-09-10 18:48:33 +0300

    right and left works

--- src/game.c
+++ src/game.c
@@ -178,7 +178,6 @@ void move_left(t_minimap *data)
 	mlx_delete_image(data->mlx, data->background_png);
 	data->background_png = mlx_texture_to_image(data->mlx, data->background_tex);
 	mlx_image_to_window(data->mlx, data->background_png, 0, 0);
-	// data->player->x
 	data->offsetx = data->offsetx - 3;
 	if (data->offsetx < 0)
 	{
@@ -194,7 +193,14 @@ void move_right(t_minimap *data)
 	mlx_delete_image(data->mlx, data->background_png);
 	data->background_png = mlx_texture_to_image(data->mlx, data->background_tex);
 	mlx_image_to_window(data->mlx, data->background_png, 0, 0);
-	// data->offsetx = data->offsetx + 12;
+	data->offsetx = data->offsetx + 3;
+	if (data->offsetx > 25)
+	{
+		data->player->x = data->player->x + 1;
+		data->offsetx = data->offsetx - 25;
+	}
+	draw_wall(data);
+	draw_player(data);
 	draw_wall(data);
 	draw_player(data);
 }
