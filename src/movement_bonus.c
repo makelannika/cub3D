167ec7ab999bc60de5f23a9983c1b95e555c5dde
Annika Mäkelä <makelannika@gmail.com>
2024-09-19 18:40:07 +0300

    wall collisions

---
+++ src/movement_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amakela <amakela@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 17:13:55 by amakela           #+#    #+#             */
+/*   Updated: 2024/09/19 17:51:46 by amakela          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/cub3d.h"
+
+/*bonus with wall collisions*/
+void	move_left(t_cub3d *data)
+{
+	if (data->map.offsetx < 3
+		&& data->map.grid[data->map.player.y][data->map.player.x - 1] == '1')
+		return ;
+	mlx_delete_image(data->mlx, data->minimap);
+	data->minimap = mlx_texture_to_image(data->mlx, data->minimap_txtr);
+	mlx_image_to_window(data->mlx, data->minimap, 0, 0);
+	data->map.offsetx -= 3;
+	data->map.player.pix_x -= 3;
+	if (data->map.offsetx < 0)
+	{
+		data->map.player.x -= 1;
+		data->map.offsetx += 25;
+	}
+	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
+	draw_player(data, data->map.p_angle);
+}
+
+void	move_right(t_cub3d *data)
+{
+    if (data->map.offsetx > 22
+		&& data->map.grid[data->map.player.y][data->map.player.x + 1] == '1')
+		return ;
+	mlx_delete_image(data->mlx, data->minimap);
+	data->minimap = mlx_texture_to_image(data->mlx, data->minimap_txtr);
+	mlx_image_to_window(data->mlx, data->minimap, 0, 0);
+	data->map.offsetx += 3;
+	data->map.player.pix_x += 3;
+	if (data->map.offsetx > 25)
+	{
+		data->map.player.x += 1;
+		data->map.offsetx -= 25;
+	}
+	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
+	draw_player(data, data->map.p_angle);
+}
+
+void	move_down(t_cub3d *data)
+{
+    if (data->map.offsety > 22
+		&& data->map.grid[data->map.player.y + 1][data->map.player.x] == '1')
+		return ;
+	mlx_delete_image(data->mlx, data->minimap);
+	data->minimap = mlx_texture_to_image(data->mlx, data->minimap_txtr);
+	mlx_image_to_window(data->mlx, data->minimap, 0, 0);
+	data->map.offsety += 3;
+	data->map.player.pix_y += 3;
+	if (data->map.offsety > 25)
+	{
+		data->map.player.y += 1;
+		data->map.offsety -= 25;
+	}
+	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
+	draw_player(data, data->map.p_angle);
+}
+
+void	move_up(t_cub3d *data)
+{
+    if (data->map.offsety < 3
+		&& data->map.grid[data->map.player.y - 1][data->map.player.x] == '1')
+		return ;
+	mlx_delete_image(data->mlx, data->minimap);
+	data->minimap = mlx_texture_to_image(data->mlx, data->minimap_txtr);
+	mlx_image_to_window(data->mlx, data->minimap, 0, 0);
+	data->map.offsety -= 3;
+	data->map.player.pix_y -= 3;
+	if (data->map.offsety < 0)
+	{
+		data->map.player.y -= 1;
+		data->map.offsety += 25;
+	}
+	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
+	draw_player(data, data->map.p_angle);
+}
