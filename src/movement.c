158d2ce300aa6d9873f7539319203a64c96cfe84
michael1nguyen1 <michael22nguyen@gmail.com>
2024-10-09 18:39:25 +0300

    don't know

--- src/movement.c
+++ src/movement.c
@@ -12,8 +12,41 @@
 
 #include "../include/cub3d.h"
 
+void	move(t_cub3d *data)
+{
+	printf("dir x is %f dir y is %f\n", data->ray_c.dir_x, data->ray_c.dir_y);
+	data->map.player.pix_x += data->ray_c.dir_x * 3;
+	data->map.player.pix_y += data->ray_c.dir_y * 3;
+	data->map.offsetx += data->ray_c.dir_x * 3;
+	data->map.offsety += data->ray_c.dir_y * 3;
+	if (data->map.offsety > 25)
+	{
+		data->map.player.y += 1;
+		data->map.offsety -= 25;
+	}
+	else
+	{
+		data->map.player.y -= 1;
+		data->map.offsety += 25;
+	}
+	if (data->map.offsetx > 25)
+	{
+		data->map.player.x += 1;
+		data->map.offsetx -= 25;
+	}
+	else
+	{
+		data->map.player.x -= 1;
+		data->map.offsetx += 25;
+	}
+	reset_minimap(data);
+	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
+	draw_player(data, data->map.p_angle);
+}
+
 void	move_left(t_cub3d *data)
 {
+	printf("dir x is %f dir y is %f\n", data->ray_c.dir_x, data->ray_c.dir_y);
 	if (data->map.offsetx < 3
 		&& data->map.grid[data->map.player.y][data->map.player.x - 1] == '1')
 		return ;
@@ -40,11 +73,11 @@ void	move_right(t_cub3d *data)
 	data->map.player.pix_x += 3;
 	// data->map.offsetx += (int)data->ray_c.dir_x * 3;
 	// data->map.player.pix_x += data->ray_c.dir_x * 3;
-	// if (data->map.offsetx > 25)
-	// {
-	// 	data->map.player.x += 1;
-	// 	data->map.offsetx -= 25;
-	// }
+	if (data->map.offsetx > 25)
+	{
+		data->map.player.x += 1;
+		data->map.offsetx -= 25;
+	}
 	reset_minimap(data);
 	draw_minimap(data, data->map.player.y - 5, data->map.player.x -5);
 	draw_player(data, data->map.p_angle);
@@ -59,11 +92,11 @@ void	move_down(t_cub3d *data)
 	data->map.player.pix_y += 3;
 	// data->map.offsety -= (int)data->ray_c.dir_y * 3;
 	// data->map.player.pix_y -= data->ray_c.dir_y * 3;
-	// if (data->map.offsety > 25)
-	// {
-	// 	data->map.player.y += 1;
-	// 	data->map.offsety -= 25;
-	// }
+	if (data->map.offsety > 25)
+	{
+		data->map.player.y += 1;
+		data->map.offsety -= 25;
+	}
 	// data->map.offsetx -= (int)data->ray_c.dir_x * 3;
 	// data->map.player.pix_x -= data->ray_c.dir_x * 3;
 	// if (data->map.offsetx > 25)
@@ -85,11 +118,11 @@ void	move_up(t_cub3d *data)
 	data->map.player.pix_y -= 3;
 	// data->map.offsety += (int)data->ray_c.dir_y * 3;
 	// data->map.player.pix_y += data->ray_c.dir_y * 3;
-	// if (data->map.offsety < 0)
-	// {
-	// 	data->map.player.y -= 1;
-	// 	data->map.offsety += 25;
-	// }
+	if (data->map.offsety < 0)
+	{
+		data->map.player.y -= 1;
+		data->map.offsety += 25;
+	}
 	// data->map.offsetx += (int)data->ray_c.dir_x * 3;
 	// data->map.player.pix_x += data->ray_c.dir_x * 3;
 	// if (data->map.offsetx < 0)
@@ -115,10 +148,14 @@ void	my_keyhook(mlx_key_data_t keydata, void *game_data)
 		rotate_left(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_W))
 		move_up(data);
+		// move(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_A))
 		move_left(data);
+		// move(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_D))
 		move_right(data);
+		// move(data);
 	if (mlx_is_key_down(data->mlx, MLX_KEY_S))
 		move_down(data);
+		// move(data);
 }
